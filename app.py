95005d6835ca230d6aee5f1533bc645df67ab66d
Atanas Yotov <116916211+Nask67@users.noreply.github.com>
2025-12-16 17:21:27 +0200

    Update city image URLs to Unsplash links

--- app.py
+++ app.py
@@ -168,67 +168,74 @@ CITY_COORDS = {
 }
 
 CITY_IMAGES = {
-    "Берлин": "https://images.unsplash.com/photo-1467269204594-9661b134dd2b",
-    "Мюнхен": "https://images.unsplash.com/photo-1547970810-dc1eac37d174",
-    "Хамбург": "https://images.unsplash.com/photo-1526481280690-7ead52c26d60",
-    "Кьолн": "https://images.unsplash.com/photo-1565099824688-e93eb20fe622",
+    "Берлин": "https://unsplash.com/s/photos/berlin-city",
+    "Мюнхен": "https://unsplash.com/s/photos/munich-city",
+    "Хамбург": "https://unsplash.com/s/photos/hamburg-city",
+    "Кьолн": "https://unsplash.com/s/photos/cologne-city",
 
-    "Париж": "https://images.unsplash.com/photo-1502602898657-3e91760cbb34",
-    "Лион": "https://images.unsplash.com/photo-1582555172866-f73bb12a2ab3",
-    "Марсилия": "https://images.unsplash.com/photo-1505765050516-f72dcac9c60b",
-    "Ница": "https://images.unsplash.com/photo-1508599589920-14cfa1c1fe4d",
+    "Париж": "https://unsplash.com/s/photos/paris-city",
+    "Лион": "https://unsplash.com/s/photos/lyon-city",
+    "Марсилия": "https://unsplash.com/s/photos/marseille-city",
+    "Ница": "https://unsplash.com/s/photos/nice-france",
 
-    "Рим": "https://images.unsplash.com/photo-1525874684015-58379d421a52",
-    "Флоренция": "https://images.unsplash.com/photo-1549880338-65ddcdfd017b",
-    "Венеция": "https://images.unsplash.com/photo-1504196606672-aef5c9cefc92",
-    "Милано": "https://images.unsplash.com/photo-1529260830199-42c24126f198",
+    "Рим": "https://unsplash.com/s/photos/rome-city",
+    "Флоренция": "https://unsplash.com/s/photos/florence-city",
+    "Венеция": "https://unsplash.com/s/photos/venice-city",
+    "Милано": "https://unsplash.com/s/photos/milan-city",
 
-    "Барселона": "https://images.unsplash.com/photo-1505739775308-4b4b0f1f6f9f",
-    "Мадрид": "https://images.unsplash.com/photo-1543783207-ec64e4d95325",
-    "Валенсия": "https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf",
-    "Севиля": "https://images.unsplash.com/photo-1528909514045-2fa4ac7a08ba",
+    "Барселона": "https://unsplash.com/s/photos/barcelona-city",
+    "Мадрид": "https://unsplash.com/s/photos/madrid-city",
+    "Валенсия": "https://unsplash.com/s/photos/valencia-city",
+    "Севиля": "https://unsplash.com/s/photos/seville-city",
 
-    "Солун": "https://images.unsplash.com/photo-1601297183305-6df142704ea2",
-    "Атина": "https://images.unsplash.com/photo-1505731132164-cca903cf6d90",
-    "Санторини": "https://images.unsplash.com/photo-1507537297725-24a1c029d3ca",
+    "Солун": "https://unsplash.com/s/photos/thessaloniki-city",
+    "Атина": "https://unsplash.com/s/photos/athens-city",
+    "Санторини": "https://unsplash.com/s/photos/santorini",
 
-    "Виена": "https://images.unsplash.com/photo-1526481280690-7ead52c26d60",
-    "Залцбург": "https://images.unsplash.com/photo-1564419320461-6870880221ad",
-    "Инсбрук": "https://images.unsplash.com/photo-1508614999368-9260051291ea",
+    "Виена": "https://unsplash.com/s/photos/vienna-city",
+    "Залцбург": "https://unsplash.com/s/photos/salzburg-city",
+    "Инсбрук": "https://unsplash.com/s/photos/innsbruck-city",
 
-    "Прага": "https://images.unsplash.com/photo-1505761671935-60b3a7427bad",
-    "Бърно": "https://images.unsplash.com/photo-1600628422019-8c4a0c90e2c2",
+    "Прага": "https://unsplash.com/s/photos/prague-city",
+    "Бърно": "https://unsplash.com/s/photos/brno-city",
 
-    "Амстердам": "https://images.unsplash.com/photo-1505761671935-60b3a7427bad",
-    "Ротердам": "https://images.unsplash.com/photo-1549880338-65ddcdfd017b",
+    "Амстердам": "https://unsplash.com/s/photos/amsterdam-city",
+    "Ротердам": "https://unsplash.com/s/photos/rotterdam-city",
 
-    "Стокхолм": "https://images.unsplash.com/photo-1509356843151-3e7d96241e11",
-    "Гьотеборг": "https://images.unsplash.com/photo-1588594140035-4c07f3f47c56",
+    "Стокхолм": "https://unsplash.com/s/photos/stockholm-city",
+    "Гьотеборг": "https://unsplash.com/s/photos/gothenburg-city",
 
-    "Загреб": "https://images.unsplash.com/photo-1598887142488-5f8cdbb0e1b1",
-    "Сплит": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Дубровник": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    
-    "Лисабон": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Порто": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Фаро": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Варшава": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Краков": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Гданск": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Будапеща": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Дебрецен": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Цюрих": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Женева": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Берн": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Брюксел": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Брюж": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Антверпен": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Букурещ": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Брашов": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Клуж-Напока": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Копенхаген": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
-    "Орхус": "https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb",
+    "Загреб": "https://unsplash.com/s/photos/zagreb-city",
+    "Сплит": "https://unsplash.com/s/photos/split-city",
+    "Дубровник": "https://unsplash.com/s/photos/dubrovnik-city",
+
+    "Лисабон": "https://unsplash.com/s/photos/lisbon-city",
+    "Порто": "https://unsplash.com/s/photos/porto-city",
+    "Фаро": "https://unsplash.com/s/photos/faro-portugal",
+
+    "Варшава": "https://unsplash.com/s/photos/warsaw-city",
+    "Краков": "https://unsplash.com/s/photos/krakow-city",
+    "Гданск": "https://unsplash.com/s/photos/gdansk-city",
+
+    "Будапеща": "https://unsplash.com/s/photos/budapest-city",
+    "Дебрецен": "https://unsplash.com/s/photos/debrecen-city",
+
+    "Цюрих": "https://unsplash.com/s/photos/zurich-city",
+    "Женева": "https://unsplash.com/s/photos/geneva-city",
+    "Берн": "https://unsplash.com/s/photos/bern-city",
+
+    "Брюксел": "https://unsplash.com/s/photos/brussels-city",
+    "Брюж": "https://unsplash.com/s/photos/bruges-city",
+    "Антверпен": "https://unsplash.com/s/photos/antwerp-city",
+
+    "Букурещ": "https://unsplash.com/s/photos/bucharest-city",
+    "Брашов": "https://unsplash.com/s/photos/brasov-city",
+    "Клуж-Напока": "https://unsplash.com/s/photos/cluj-napoca-city",
+
+    "Копенхаген": "https://unsplash.com/s/photos/copenhagen-city",
+    "Орхус": "https://unsplash.com/s/photos/aarhus-city",
 }
+
 
 
 
